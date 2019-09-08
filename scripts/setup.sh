2986db82248948519bcba22fa9ec240680af2115
francescodelduchetto <francescodelduchetto@gmail.com>
2019-09-08 13:56:41 +0100

    Saturday experiments

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,8 @@ ws_root=$(readlink -f "$this_dir/../../..")
 export DISPLAY=:0
 export ROBOT_NAME=linda
 export LINDA_WS="$ws_root"
-export ROS_IP="10.82.0.70"
+#export ROS_IP="10.82.0.70"
+export ROS_IP="127.0.0.1"
 export ROS_MASTER_URI="http://"$ROS_IP":11311"
 
 
