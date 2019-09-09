ba162f29f93bb2e5f1b3e80a1d130955c17bf37f
francescodelduchetto <francescodelduchetto@gmail.com>
2019-09-09 10:16:50 +0100

    mch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,23 +13,27 @@ echo $LINDA_WS
 
 # VARs likely to change or depend on the scenario.
 
-export ROS_IP="10.82.0.70"
+#export ROS_IP="10.82.0.70"
+export ROS_IP="127.0.0.1"
 export ROS_MASTER_URI="http://"$ROS_IP":11311"
 
-TESTCODE=INB3123
-LOC_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3123/map_real_sci/cropped.yaml"
-NAV_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3123/map_real_sci_mcdm/cropped.yaml"
-STARTING_POSE_PY="-5.585"
-STARTING_POSE_PX="-8.798"
-STARTING_POSE_OZ="-0.999"
-STARTING_POSE_OW="0.012"
+#TESTCODE=INB3123
+#LOC_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3123/map_real_sci/cropped.yaml"
+#NAV_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3123/map_real_sci_mcdm/cropped.yaml"
+#STARTING_POSE_PX="-5.585"
+#STARTING_POSE_PY="-8.798"
+#STARTING_POSE_OZ="-0.999"
+#STARTING_POSE_OW="0.012"
 
 # ........................................................................................
-#TESTCODE=INB3ENG
-# LOC_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3eng/scitos_map_real/map.yaml"
-# NAV_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3eng/scitos_map_real_mcdm/map.yaml"
+TESTCODE=INB3ENG
+LOC_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3eng/scitos_map_real/map.yaml"
+NAV_MAP_YAML="$(rospack find mcdm_experiments)/maps/inb3eng/scitos_map_real_mcdm/map.yaml"
 
-# STARTING_POSE_PY="-33.70"
-# STARTING_POSE_PX="6.36"
-# STARTING_POSE_OZ="0.682"
-# STARTING_POSE_OW="0.731"
+PARAM_1=0.2
+PARAM_2=0.6
+PARAM_3=0.2
+STARTING_POSE_PX="-33.6"
+STARTING_POSE_PY="8.3"
+STARTING_POSE_OZ="0.682"
+STARTING_POSE_OW="0.731"
