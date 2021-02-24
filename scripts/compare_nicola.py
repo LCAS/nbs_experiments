c4a9d615b7e12543239af0e551a3c372729c6ffd
francescodelduchetto <francescodelduchetto@gmail.com>
2021-02-24 09:46:41 +0000

    scripts to read robag wiuth multipickers

--- scripts/compare_nicola.py
+++ scripts/compare_nicola.py
@@ -5,7 +5,7 @@ import argparse
 from matplotlib.ticker import MaxNLocator
 
 
-def plotDistance(data, pos, y_label, label, color, axes):
+def plotDistance(data, pos, y_label, label, color, style, axes):
     #  Plot the trajectories
     # Now data are plotted for every seconds, we may want to subsample them for every minute
     # print(data.shape)
@@ -14,25 +14,31 @@ def plotDistance(data, pos, y_label, label, color, axes):
     # data = data[0::60]
     # print(data.shape)
     x = np.arange(0, data.shape[0], 1, dtype=float) / 6.0
-    axs[pos].plot(x, data[:, 0], label=label, color=color)
-    axs[pos].fill_between(x, data[:, 0]-data[:, 1], data[:, 0] +
-                          data[:, 1], alpha=0.05, edgecolor=color, facecolor=color)
+    if label is not None and not label.startswith("picker"):
+        axs[pos].plot(x[::3], data[::3, 0], label=label, color=color)
+        axs[pos].fill_between(x[::3], data[::3, 0]-data[::3, 1], data[::3, 0] +
+                            data[::3, 1], alpha=0.05, edgecolor=color, facecolor=color)
+    else:
+        axs[pos].plot(x[::2], data[::2, 0], label=label, color=color, linestyle=style)
     axs[pos].set_ylabel(y_label, fontsize=14)
     axs[pos].xaxis.set_major_locator(MaxNLocator(integer=True))
     axs[pos].set_xlim(min(x), max(x))
-    axs[pos].set_ylim(-1, 35)
+    axs[pos].set_ylim(0, 12)
+    axs[pos].tick_params(axis='both', which='major', labelsize=14)
+    axs[pos].tick_params(axis='both', which='minor', labelsize=14)
     if (pos == 1):
-        axs[pos].set_ylim(-1, 15)
+        axs[pos].set_ylim(0, 35)
+    
 
 
-def calculateMetricsAndPlot(data, ylabel, label_list, color_list, axs, axs_pos):
+def calculateMetricsAndPlot(data, ylabel, label_list, color_list, style, axs, axs_pos):
     # Calculate metrics (MSE)
-    if (axs_pos == 0):
+    if (axs_pos == 1):
         print("=== TOPOLOGICAL DISTANCE ===")
     else:
         print("=== EUCLIDEAN DISTANCE ===")
     for i in range(len(data)):
-        print("----" + label_list[i] + "----")
+        print("----" + str(label_list[i]) + "----")
         mean_error = round(np.nanmean(data[i][:, 0]), 2)
         variance = round(np.nanstd(data[i][:, 0]), 2)
         print("     Mean error: " + str(mean_error) + "(" + str(variance) + ")")
@@ -41,7 +47,7 @@ def calculateMetricsAndPlot(data, ylabel, label_list, color_list, axs, axs_pos):
         # plotDistance(data[i][:,0,:], pos=0, y_label="X-error[m]",          label=label_list[i], color=color_list[i], axes=axs)
         # plotDistance(data[i][:,1,:], pos=1, y_label="Y-error[m]",          label=label_list[i], color=color_list[i], axes=axs)
         plotDistance(data[i], pos=axs_pos, y_label=ylabel,
-                     label=label_list[i], color=color_list[i], axes=axs)
+                     label=label_list[i], color=color_list[i], style=style, axes=axs)
 
 
 def normalizeLenData(data):
@@ -89,10 +95,23 @@ if __name__ == "__main__":
     fig = plt.figure(figsize=(12, 8))
     axs = fig.subplots(rows, cols, sharex=True, sharey=False)
     # fig.suptitle("Tags localization error")
+
     plt.xlabel("Minutes", fontsize=14)
-    label_list = ["TPF", "BayesFilter", ]
-    color_list = ["b", "r", "#6666ff", "#ff6666"]
+    label_list = ["LIDAR+GPS+RFID(ours)", "Dondrup et al.[24]"]
+    pickers_label_list = [
+        ["picker1", "picker1"],
+        ["picker2", "picker2"],
+        ["picker3", "picker3"]
+    ]
+    color_list = ["g", "b"]
+    pickers_color_list = ["#6666ff66", "#ff666666"]
+    pickers_style = ["--", "-.", ":"]
+    tot_topo_combined_result = []
+    tot_topo_estimated_node_result = []
+    tot_combined_result = []
+    tot_estimated_node_result = []
     for tagi in range(3):
+            
         topo_combined_result = np.load(args.root + "topo_result{}.npy".format(tagi))
         topo_estimated_node_result = np.load(
             args.root + "bayes_topo_result{}.npy".format(tagi))
@@ -101,7 +120,16 @@ if __name__ == "__main__":
         estimated_node_result = np.load(
             args.root + "bayes_metric_result{}.npy".format(tagi))
 
-
+        # if len(tot_topo_combined_result) == 0:
+        #     tot_topo_combined_result = topo_combined_result
+        #     tot_topo_estimated_node_result = topo_estimated_node_result
+        #     tot_combined_result = combined_result
+        #     tot_estimated_node_result = estimated_node_result
+        # else:
+        #     tot_topo_combined_result = np.hstack((tot_topo_combined_result, topo_combined_result))
+        #     tot_topo_estimated_node_result = np.hstack((tot_topo_estimated_node_result, topo_estimated_node_result))
+        #     tot_combined_result = np.hstack((tot_combined_result, combined_result))
+        #     tot_estimated_node_result = np.hstack((tot_estimated_node_result, estimated_node_result))
 
         # TODO: Check that they sizes are the sames
         # data = [gps_connected_result, gps_unconnected_result, lidar_result, rfid_result, combined_result]
@@ -113,14 +141,38 @@ if __name__ == "__main__":
         topo_data = normalizeLenData(topo_data)
         metric_data = normalizeLenData(metric_data)
 
-        calculateMetricsAndPlot(
-            topo_data, "Topological Error[nodes]", label_list, color_list, axs, 0)
-        calculateMetricsAndPlot(
-            metric_data, "Euclidan Error[m]", label_list, color_list, axs, 1)
+        # calculateMetricsAndPlot(
+        #     topo_data, "Topological Error[nodes]", pickers_label_list[tagi], pickers_color_list, pickers_style[tagi], axs, 0)
+        # calculateMetricsAndPlot(
+        #     metric_data, "Euclidan Error[m]", pickers_label_list[tagi], pickers_color_list, pickers_style[tagi], axs, 1)
+        
+    tot_topo_combined_result = np.load(
+        args.root + "topo_result{}.npy".format("tot"))
+    tot_topo_estimated_node_result = np.load(
+        args.root + "bayes_topo_result{}.npy".format("tot"))
+
+    tot_combined_result = np.load(
+        args.root + "metric_result{}.npy".format("tot"))
+    tot_estimated_node_result = np.load(
+        args.root + "bayes_metric_result{}.npy".format("tot"))
+    # TODO: Check that they sizes are the sames
+    # data = [gps_connected_result, gps_unconnected_result, lidar_result, rfid_result, combined_result]
+    topo_data = [tot_topo_combined_result, tot_topo_estimated_node_result]
+    metric_data = [tot_combined_result, tot_estimated_node_result]
+    # label_list=["GPS-connected", "GPS-unconnected", "Lidar", "RFID", "Combined"]
+
+    topo_data = normalizeLenData(topo_data)
+    metric_data = normalizeLenData(metric_data)
+
+    calculateMetricsAndPlot(
+        metric_data, "Euclidan Error[m]", label_list, color_list, "-", axs, 0)
+    calculateMetricsAndPlot(
+        topo_data, "Topological Error[nodes]", label_list, color_list, "-", axs, 1)
+
 
     # plt.show()
     fig.tight_layout()
     # , bbox_to_anchor=(0.5, 0.95))
     axs[0].legend(ncol=2, loc='upper right', fontsize=12)
 
-    fig.savefig(fname=os.path.join(out_folder, "distance.png"), dpi=300)
+    fig.savefig(fname=os.path.join(out_folder, "distance.pdf"), dpi=300)
--- scripts/plot_bayes.py
+++ scripts/plot_bayes.py
@@ -298,4 +298,36 @@ if __name__ == "__main__":
         result = np.concatenate((bayestopo_averages, bayestopo_stds), axis=1)
         np.save(out_folder + "/bayes_topo_result{}".format(tagi), result)
 
+    averages = np.nanmean(all_bags_distances, axis=0)
+    stds = np.nanstd(all_bags_distances, axis=0)
+
+    topo_averages = np.nanmean(all_bags_topo_distances, axis=0)
+    topo_stds = np.nanstd(all_bags_topo_distances, axis=0)
+
+    bayesaverages = np.nanmean(all_bags_bayesdistances, axis=0)
+    bayesstds = np.nanstd(all_bags_bayesdistances, axis=0)
+    bayestopo_averages = np.nanmean(
+        all_bags_topo_bayesdistances, axis=0)
+    bayestopo_stds = np.nanstd(
+        all_bags_topo_bayesdistances, axis=0)
+
+    averages = np.expand_dims(averages, axis=1)
+    stds = np.expand_dims(stds, axis=1)
+    result = np.concatenate((averages, stds), axis=1)
+    np.save(out_folder + "/metric_result{}".format("tot"), result)
+
+    topo_averages = np.expand_dims(topo_averages, axis=1)
+    topo_stds = np.expand_dims(topo_stds, axis=1)
+    result = np.concatenate((topo_averages, topo_stds), axis=1)
+    np.save(out_folder + "/topo_result{}".format("tot"), result)
+
+    bayesaverages = np.expand_dims(bayesaverages, axis=1)
+    bayesstds = np.expand_dims(bayesstds, axis=1)
+    result = np.concatenate((bayesaverages, bayesstds), axis=1)
+    np.save(out_folder + "/bayes_metric_result{}".format("tot"), result)
+
+    bayestopo_averages = np.expand_dims(bayestopo_averages, axis=1)
+    bayestopo_stds = np.expand_dims(bayestopo_stds, axis=1)
+    result = np.concatenate((bayestopo_averages, bayestopo_stds), axis=1)
+    np.save(out_folder + "/bayes_topo_result{}".format("tot"), result)
 
